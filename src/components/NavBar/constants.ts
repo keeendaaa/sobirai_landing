aedbbe7f846b7a707da55cad1724ed9dd11db9d2
keeendaaa <122747995+keeendaaa@users.noreply.github.com>
2026-04-28 23:45:37 +0500

    Replace landing with deployed static build

--- src/components/NavBar/constants.ts
+++
@@ -1,7 +0,0 @@
-import type { NavigationItem } from "./NavBar";
-
-export const marketingNavigationItems: NavigationItem[] = [
-  { name: "Продукт", to: "#product" },
-  { name: "Подписки", to: "#subscriptions" },
-  { name: "Кейсы", to: "#cases" },
-] as const;
